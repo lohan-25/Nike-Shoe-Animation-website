704d55884d407597f6d3629261a3eec948134105
Your Name <slohan21@gmail.com>
2024-01-11 15:44:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+var tl = gsap.timeline();
+gsap.set(".a",{opacity:0, y: 10});
+gsap.set("#right img",{opacity:0, y: 10,scale: 1.6});
+tl
+.from("#left",{
+    width: 0,
+    ease: Expo.easeInOut,
+    duration: 1.5
+})
+
+.from("#right",{
+    width: 0,
+    ease: Expo.easeInOut,
+    duration: 1.5
+})
+
+.to(".a",{
+    delay: -1,
+    stagger: .1,
+    opacity: 1,
+    y: 0,
+    ease: Expo.easeInOut,
+    duration: 2
+})
+
+.to("#right img",{
+    delay: -1,
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    ease: Expo.easeInOut,
+    duration: 2
+})
